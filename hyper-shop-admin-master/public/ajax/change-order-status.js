739c7f7c535d0af9f2f2105f18dd619ed0be5f0d
ToanDinh <Toandinh01675@gmail.com>
2022-11-21 17:07:23 +0700

    talk later

--- hyper-shop-admin-master/public/ajax/change-order-status.js
+++ hyper-shop-admin-master/public/ajax/change-order-status.js
@@ -1,42 +1,45 @@
-const url = "/api/order/change-status"
+const url = "/api/order/change-status";
 
-const changeStatusHandler=async(e)=>{
-    let statusStr = e.currentTarget.id;
-    const index = statusStr.indexOf('-');
-    const id = statusStr.substring(index+1);
-    const status = statusStr.substring(0,index);
-    const itemStatus ={
-        id:id,
-        status: status,
-    }
+const changeStatusHandler = async (e) => {
+  let statusStr = e.currentTarget.id;
+  const index = statusStr.indexOf("-");
+  const id = statusStr.substring(index + 1);
+  const status = statusStr.substring(0, index);
+  const itemStatus = {
+    id: id,
+    status: status,
+  };
 
-    let currentStatus = e.currentTarget.closest('.dropdown-menu').getAttribute("data-status");
-    if(currentStatus!=status){
-        await fetch(url,{
-            method:"POST",
-            body: JSON.stringify(itemStatus),
-            headers:{
-                'Content-type':'application/json; charset=utf-8'
+  let currentStatus = e.currentTarget
+    .closest(".dropdown-menu")
+    .getAttribute("data-status");
+  if (currentStatus != status) {
+    await fetch(url, {
+      method: "POST",
+      body: JSON.stringify(itemStatus),
+      headers: {
+        "Content-type": "application/json; charset=utf-8",
+      },
+    })
+      .then((response) => {
+        if (response.status >= 200 && response.status < 300) {
+          return response.json().then((order) => {
+            const oldRow = document.getElementById(id);
+            oldRow.remove();
+            let table;
+            if (order.status == "Delivering") {
+              table = document.getElementById("delivering-table");
             }
-        }).then(response=>{
-            if (response.status>= 200 && response.status<300){
-                return response.json().then(order=>{
-                    const oldRow = document.getElementById(id);
-                    oldRow.remove();
-                    let table;
-                    if(order.status=="Delivering"){
-                        table = document.getElementById('delivering-table');
-                    }
-                    if(order.status=="Delivered"){
-                        table = document.getElementById('delivered-table');
-                    }
-                    if(order.status=="Pending"){
-                        table = document.getElementById('pending-table');
-                    }
-                    const tableBody = table.querySelector('tbody');
-                    const row = document.createElement('tr');
-                    row.setAttribute('id',order._id);
-                    row.innerHTML=`
+            if (order.status == "Delivered") {
+              table = document.getElementById("delivered-table");
+            }
+            if (order.status == "Pending") {
+              table = document.getElementById("pending-table");
+            }
+            const tableBody = table.querySelector("tbody");
+            const row = document.createElement("tr");
+            row.setAttribute("id", order._id);
+            row.innerHTML = `
                         <th scope="row">${order._id}</th>
                         <td>${order.user.email}</td>
                         <td>${order.phone}</td>
@@ -58,28 +61,29 @@ const changeStatusHandler=async(e)=>{
                         <td>
                             <a href="/orders/${order._id}" class="btn btn-primary">Details</a>
                         </td>
-                    `
-                    alert("Change order status successfully.");
-                    tableBody.appendChild(row);
+                    `;
+            alert("Thay đổi trạng thái đơn hàng thành công !");
+            tableBody.appendChild(row);
 
-                    const oldScript = document.getElementById('rerun-script');
-                    const newScript = document.createElement('script');
-                    newScript.setAttribute('src', '/js/custom/rerun-change-orderstatus.js');
-                    newScript.setAttribute('id', 'rerun-script');
-
-                    oldScript.parentNode.insertBefore(newScript, oldScript);
-                    oldScript.parentElement.removeChild(oldScript);
-                } 
-                
+            const oldScript = document.getElementById("rerun-script");
+            const newScript = document.createElement("script");
+            newScript.setAttribute(
+              "src",
+              "/js/custom/rerun-change-orderstatus.js"
             );
-            }else{
-                response.json().then(error=>{
-                    console.log('ERROR: '+error);
-                });
-            }
-        }).catch(error=>{
-            console.log(error);
-        })
-    }
-}
+            newScript.setAttribute("id", "rerun-script");
 
+            oldScript.parentNode.insertBefore(newScript, oldScript);
+            oldScript.parentElement.removeChild(oldScript);
+          });
+        } else {
+          response.json().then((error) => {
+            console.log("ERROR: " + error);
+          });
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+};
